9b797d6f4d346e7d6b68e4ab5cd42719aa0afdd8
Tejas Kannan <tkannan@bluefish.a.cs.uchicago.edu>
2021-10-08 17:43:50 -0500

    Include most frequent label in attack accuracy plots

--- privddnn/analysis/plot_attack.py
+++ privddnn/analysis/plot_attack.py
@@ -1,15 +1,25 @@
+import os
 import matplotlib.pyplot as plt
+import numpy as np
 from argparse import ArgumentParser
 
 from privddnn.utils.file_utils import read_json_gz
 from privddnn.utils.plotting import to_label, COLORS, AXIS_FONT, TITLE_FONT, LEGEND_FONT
 from privddnn.utils.plotting import LINE_WIDTH, MARKER, MARKER_SIZE, PLOT_STYLE
+from privddnn.dataset.dataset import Dataset
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--test-log', type=str, required=True)
     parser.add_argument('--output-file', type=str)
     args = parser.parse_args()
+
+    # Get the most frequent label
+    tokens = args.test_log.split(os.sep)
+    dataset = Dataset(tokens[-3])
+    labels = dataset.get_test_labels()
+    label_freq = np.bincount(labels) / len(labels)
+    most_freq = np.max(label_freq) * 100.0
 
     # Read the attack accuracy
     test_log = read_json_gz(args.test_log)
@@ -30,6 +40,8 @@ if __name__ == '__main__':
                     markersize=MARKER_SIZE,
                     linewidth=LINE_WIDTH,
                     color=COLORS[policy_name])
+
+        ax.axhline(y=most_freq, color='r')
 
         ax.set_xlabel('Frac Stopping at 2nd Output', fontsize=AXIS_FONT)
         ax.set_ylabel('Test Accuracy (%)', fontsize=AXIS_FONT)
